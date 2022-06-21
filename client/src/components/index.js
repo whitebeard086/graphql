b2932a320467753485468ea54623b7bf5520327b
Mr. Alex <whitebeard086@gmail.com>
2022-06-21 05:11:22 +0100

    edit project form

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,6 +5,7 @@ export { default as Header } from "./Header";
 export { default as ClientRow } from "./ClientRow";
 export { default as Clients } from "./Clients";
 export { default as DeleteProjectButton } from "./DeleteProjectButton";
+export { default as EditProjectForm } from "./EditProjectForm";
 export { default as ProjectCard } from "./ProjectCard";
 export { default as Projects } from "./Projects";
 export { default as Spinner } from "./Spinner";
